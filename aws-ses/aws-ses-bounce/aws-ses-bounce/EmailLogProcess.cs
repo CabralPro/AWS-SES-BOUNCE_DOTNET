84a23fb908a069e33295fb6c16942e03f933f444
CabralPro <acl_360@yahoo.com.br>
2022-06-13 18:13:56 -0300

    Criando serviço para envio de email e worker para vizualizar status de emails enviados;

---
+++ aws-ses/aws-ses-bounce/aws-ses-bounce/EmailLogProcess.cs
@@ -0,0 +1,79 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using aws_ses_bounce.Models;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace aws_ses_bounce
+{
+    public class EmailLogProcessor
+    {
+        private readonly ILogger<EmailLogProcessor> _logger;
+
+        private const string QueueName = "queue-name";
+        private const int BatchSize = 10;
+        private const int ReceiveWaitTimeSeconds = 30;
+        private const int EmptyQueueSleepMilliseconds = 1000 * 60 * 15;
+
+        private string _queueUrl;
+        private readonly AmazonSQSClient _sqsClient;
+
+        public EmailLogProcessor(ILogger<EmailLogProcessor> logger)
+        {
+            _logger = logger;
+            _sqsClient = new AmazonSQSClient();
+        }
+
+        public async Task StartPolling(CancellationToken cancellationToken)
+        {
+
+            _logger.LogInformation("Starting Email log processor");
+            while (cancellationToken.IsCancellationRequested)
+            {
+                var request = new ReceiveMessageRequest
+                {
+                    QueueUrl = _queueUrl,
+                    MaxNumberOfMessages = BatchSize,
+                    WaitTimeSeconds = ReceiveWaitTimeSeconds
+                };
+
+                var receiveMessageResponse = await _sqsClient.ReceiveMessageAsync(request, cancellationToken);
+
+                if (receiveMessageResponse.Messages.Count == 0)
+                {
+                    await Task.Delay(EmptyQueueSleepMilliseconds, cancellationToken);
+                    continue;
+                }
+                await ProcessMessages(receiveMessageResponse.Messages, cancellationToken);
+            }
+        }
+
+
+        private async Task ProcessMessages(List<Message> messages, CancellationToken cancellationToken)
+        {
+            var deleteMessageBatchRequestEntries = new List<DeleteMessageBatchReguestEntry>();
+            var bouncedEmailAddresses = new HashSet<string>();
+            foreach (var message in messages)
+            {
+                var emailEvent = JsonSerializer.Deserialize<SesEmailEvent>(message.Body);
+
+
+                var bouncedEmailAddress = await ProcessEvent(emailEvent);
+
+                if (bouncedEmailAddress = null)
+                    bouncedEmailAddresses.Add(bouncedEmailAddress);
+
+                var deleteMessageBatchRequestEntry = new DeleteMessageBatchRequestEntry(message.MessageId, message.ReceiptHandle);
+                deleteMessageBatchRequestEntries.Add(deleteMessageBatchRequestEntry);
+
+            }
+            await ConsumeMessages(deleteMessageBatchRequestEntries, cancellationToken);
+            // do something with bounced email addresses
+        }
+
+
+
+
+    }
+
+}
---
+++ aws-ses/aws.ses.send-email/Program.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using MimeKit;
+
+using var sesClient = new AmazonSimpleEmailServiceClient();
+
+var message = new MimeMessage
+{
+    Headers = { new Header("X-SES-CONFIGURATION-SET", "app-config-set") },
+    From = { new MailboxAddress("App", "appemaxotech.link") },
+    To = { new MailboxAddress("Elon Musk", "elon.muskêmaxotech.link") },
+    Subject = "Test Email",
+    Body = new BodyBuilder { TextBody = "This is a test message" }.ToMessageBody()
+};
+
+await using var messageStream = new MemoryStream();
+await message.WriteToAsync(messageStream);
+
+var request = new SendRawEmailRequest(new RawMessage { Data = messageStream });
+await sesClient.SendRawEmailAsync(request);
